bf7c840d648862c05d2e9020f6fd873ea62f1742
Teo <teo@Admins-MacBook-Pro.local>
2021-07-08 15:21:49 -0500

    Login_Test_complete_successfull

--- src/test/java/PageObjects/LoginPage.java
+++ src/test/java/PageObjects/LoginPage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginPage {
 
@@ -14,16 +16,28 @@ public class LoginPage {
         PageFactory.initElements(new AppiumFieldDecorator(driver, Duration.ofSeconds(30)), this);
     }
 
+
    @FindBy(xpath = "//XCUIElementTypeStaticText[@name=\"Login\"]\n")
    public WebElement loginButton;
 
     @FindBy(xpath = "//XCUIElementTypeApplication[@name=\"PDQ Smart\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[1]/XCUIElementTypeTextField\n")
     public WebElement emailField;
 
+    @FindBy(xpath = "//XCUIElementTypeApplication[@name=\"PDQ Smart\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeSecureTextField")
+    public WebElement passwordField;
+
+    @FindBy(xpath = "//XCUIElementTypeStaticText[@name=\"Continue\"]")
+    public WebElement submitButton;
+
     public void loginFlow() throws InterruptedException {
-        loginButton.click();
-        emailField.sendKeys("fasasafasfsa");
-        Thread.sleep(4000);
+
+        this.loginButton.click();
+        List<WebElement>  elementList = Arrays.asList(this.emailField, this.passwordField);
+        //elementList.forEach(ele -> ele.isDisplayed());
+        this.emailField.sendKeys("teodoro.garcia@delphiansystems.com");
+        this.passwordField.sendKeys("Tester@22");
+        this.submitButton.click();
+        Thread.sleep(5000);
 
     }
 }
